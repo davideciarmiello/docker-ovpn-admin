1e3a7b01291ddfdda6116f3d03d8c3e11e057542
dciarmiello <dciarmiello@DESKTOP-4BH310G>
2022-07-15 12:00:52 +0200

    fixs

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 ARG FRONTEND_BUILDER_IMAGE=node:16.13.0-alpine3.12
 FROM $FRONTEND_BUILDER_IMAGE AS frontend-builder
-#RUN apk add --update python3 make g++ git && rm -rf /var/cache/apk/*
-#RUN git clone https://github.com/flant/ovpn-admin.git /app
-#RUN cd /app && git pull
 COPY ./ovpn-admin/frontend/ /app
-#RUN cd /app && npm install && npm run build
+RUN [ ! -f "/app/frontend_builded" ] && apk add --update python3 make g++ git && rm -rf /var/cache/apk/*
+RUN [ ! -f "/app/frontend_builded" ] && cd /app && npm install && npm run build
 
 ARG BACKEND_BUILDER_IMAGE=golang:1.17.3-buster
 FROM $BACKEND_BUILDER_IMAGE AS backend-builder
@@ -13,7 +11,6 @@ ARG TARGETARCH
 RUN go install github.com/gobuffalo/packr/v2/packr2@latest
 COPY --from=frontend-builder /app/static /app/frontend/static
 COPY ./ovpn-admin /app
-#COPY --from=frontend-builder /app /app
 RUN cd /app && echo files in app && ls
 RUN cd /app && packr2 && env CGO_ENABLED=1 GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -a -tags netgo -ldflags '-linkmode external -extldflags -static -s -w' -o ovpn-admin && packr2 clean
 
--- before-build.sh
+++ before-build.sh
@@ -6,13 +6,16 @@ echo script before-build.sh
 
 mkdir -p tmp
 
-FRONTEND_BUILDER_IMAGE=$(cat ovpn-admin/Dockerfile | grep "AS frontend-builder" | sed -e "s/^FROM //" -e "s/ AS .*//")
-BACKEND_BUILDER_IMAGE=$(cat ovpn-admin/Dockerfile | grep "AS backend-builder" | sed -e "s/^FROM //" -e "s/ AS .*//")
+[ ! -d "./ovpn-admin" ] && git clone https://github.com/flant/ovpn-admin.git
+[ ! -f "./ovpn-admin/Dockerfile" ] && cp ovpn-admin/Dockerfile  ovpn-admin/Dockerfilebak
+
+FRONTEND_BUILDER_IMAGE=$(cat ovpn-admin/Dockerfilebak | grep "AS frontend-builder" | sed -e "s/^FROM //" -e "s/ AS .*//")
+BACKEND_BUILDER_IMAGE=$(cat ovpn-admin/Dockerfilebak | grep "AS backend-builder" | sed -e "s/^FROM //" -e "s/ AS .*//")
 
 echo FRONTEND_BUILDER_IMAGE=${FRONTEND_BUILDER_IMAGE} >> "$GITHUB_ENV"
 echo BACKEND_BUILDER_IMAGE=${BACKEND_BUILDER_IMAGE} >> "$GITHUB_ENV"
 
-FRONTEND_BUILDER_RUN="
+FRONTEND_BUILDER_SCRIPT="
 echo Compiling frontend
 apk add --update python3 make g++ git && rm -rf /var/cache/apk/*
 cd /app && npm install && npm run build
@@ -20,7 +23,7 @@ echo 1 > frontend_builded
 echo frontend compiled
 "
 echo "#!/bin/sh" > tmp/frontend-builder-entrypoint.sh
-echo "${FRONTEND_BUILDER_RUN}" >> tmp/frontend-builder-entrypoint.sh
+echo "${FRONTEND_BUILDER_SCRIPT}" >> tmp/frontend-builder-entrypoint.sh
 
 FRONTEND_BUILDER_RUN="sh /workspace/tmp/frontend-builder-entrypoint.sh"
 
