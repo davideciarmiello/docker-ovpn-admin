7cf2f5e27766bd44b7afb04c67b927fb7756a04e
dciarmiello <dciarmiello@DESKTOP-4BH310G>
2022-07-15 13:38:39 +0200

    fixs

--- before-build.ps1
+++ before-build.ps1
@@ -1,1 +1,4 @@
- docker run --rm -v ${PWD}:/data -w /data alpine sh before-build.sh
+ if (Test-Path ./tmp/GITHUB_ENV.txt) {
+  Remove-Item ./tmp/GITHUB_ENV.txt
+}
+ docker run --rm -v ${PWD}:/data -w /data -e GITHUB_ENV=/data/tmp/GITHUB_ENV.txt alpine sh before-build.sh
